0220a0b494009bb41053ae6b922f1a9ae9b06e8f
Igor <igorizr1@gmail.com>
2014-06-19 16:09:38 +0300

    first commit

---
+++ LocalStorageAdapter.js
@@ -0,0 +1,38 @@
+angular.module('LocalStorageAdapter', [])
+.factory('LocalStorageService', function(GENERAL_CONFIG) {
+
+    var Storage = localStorage,
+        APP_NAME = (GENERAL_CONFIG.APP_NAME + "_");
+        
+    return {
+        set: function(k, v) {
+            k = (APP_NAME + k);
+            if (arguments.length !== 2)
+                return false;
+            if (v && typeof(v) !== "string") {
+                var v = JSON.stringify(v);
+            }
+            return Storage.setItem(k, v);
+        },
+        get: function(k) {
+            k = (APP_NAME + k);
+            if (arguments.length !== 1)
+                return false;
+            var result = Storage.getItem(k);
+            if (result === null)
+                return false;
+            if (result.match(/\[.*\]|\{.*\}/)) {
+                return JSON.parse(result);
+            } else {
+                return result;
+            }
+            return false;
+        },
+        clear: function(item) {
+            return (arguments.length === 1 ? Storage.removeItem(APP_NAME + item) : Storage.clear());
+        }
+    };
+
+});
+
+
