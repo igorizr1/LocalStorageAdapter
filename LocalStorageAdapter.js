1a434d20dfbf3f65f75e63c96faddbf171c89420
codestack <codestack123@gmail.com>
2014-06-30 17:05:05 +0300

    dependency of config name

--- LocalStorageAdapter.js
+++ LocalStorageAdapter.js
@@ -1,4 +1,4 @@
-angular.module('LocalStorageAdapter', [])
+angular.module('LocalStorageAdapter', ["general.config"])
 .factory('LocalStorageService', function(GENERAL_CONFIG) {
 
     var Storage = localStorage,
