60bb6e0469889424950b440a78a2ece9a368c9b3
codestack <codestack123@gmail.com>
2014-07-14 17:56:35 +0300

    added push method

--- LocalStorageAdapter.js
+++ LocalStorageAdapter.js
@@ -2,16 +2,34 @@ angular.module('LocalStorageAdapter', ["general.config"])
 .factory('LocalStorageAdapter', function(GENERAL_CONFIG) {
 
     var Storage = localStorage,
-        APP_NAME = (GENERAL_CONFIG.APP_NAME + "_");
+        APP_NAME = (GENERAL_CONFIG.APP_NAME + "_"),
+        empty = function(){
+            try {
+                if(arguments.length === 0)return true;
+                for(var i = 0; i<arguments.length; ++i)
+                    if(!is_set(arguments[i]))
+                        return true;
+                    else
+                        if(is_array(arguments[i]) && arguments[i].length === 0)
+                            return true;
+                        else if(typeof(arguments[i]) === "object" && Object.keys(arguments[i]).length === 0)
+                            return true;
+                        else if(typeof(arguments[i]) === "string" && arguments[i] === "")
+                            return true;
+                        else if(typeof(arguments[i]) === "number" && arguments[i] === "")
+                            return true;
+                return false;
+            } catch(e) {
+                return true;
+            }
+        };
         
     return {
         set: function(k, v) {
             k = (APP_NAME + k);
-            if (arguments.length !== 2)
+            if(arguments.length !== 2)
                 return false;
-            if (v && typeof(v) !== "string") {
-                var v = JSON.stringify(v);
-            }
+            if(v && typeof(v) !== "string")var v = JSON.stringify(v);
             return Storage.setItem(k, v);
         },
         get: function(k) {
@@ -19,14 +37,20 @@ angular.module('LocalStorageAdapter', ["general.config"])
             if (arguments.length !== 1)
                 return false;
             var result = Storage.getItem(k);
-            if (result === null)
-                return false;
-            if (result.match(/\[.*\]|\{.*\}/)) {
-                return JSON.parse(result);
-            } else {
-                return result;
-            }
+            if(result === null)return false;
+            return result.match(/\[.*\]|\{.*\}/)
+                ? JSON.parse(result)
+                : result;
             return false;
+        },
+        array_push : function(k, v){
+            var current_value = this.get(k);
+            if(empty(current_value))
+                current_value = [];
+            else if(!(current_value instanceof Array))
+                return false;
+            current_value.push(v);
+            return this.set(k, current_value); 
         },
         clear: function(item) {
             return (arguments.length === 1 ? Storage.removeItem(APP_NAME + item) : Storage.clear());
